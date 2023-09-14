9a507dc8422bcc4d380521903a025d3335d1006c
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-09-13 17:28:48 -0700

    fixed swagger

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -18,15 +18,6 @@ namespace sikho_backend.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+   
     }
 }
--- Controllers/IndustriesController.cs
+++ Controllers/IndustriesController.cs
@@ -4,34 +4,108 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using sikho_backend.Models;
 
 namespace sikho_backend.Controllers
 {
     [Route("[controller]")]
+    [ApiController]
     public class IndustriesController : Controller
     {
         private readonly ILogger<IndustriesController> _logger;
+        private readonly APIDbContext _context;
 
-        public IndustriesController(ILogger<IndustriesController> logger)
+        public IndustriesController(ILogger<IndustriesController> logger, APIDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
+       // GET: api/Industry
         [HttpGet]
-        public string Greetings(){
-            return "Hello there";
+        public async Task<ActionResult<IEnumerable<Industry>>> GetIndustries()
+        {
+            return await _context.Industries.ToListAsync();
         }
 
-        public IActionResult Index()
+        // GET: api/Industry/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Industry>> GetIndustry(int id)
         {
-            return View();
+            var industry = await _context.Industries.FindAsync(id);
+
+            if (industry == null)
+            {
+                return NotFound();
+            }
+
+            return industry;
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // PUT: api/Industry/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIndustry(int id, Industry industry)
         {
-            return View("Error!");
+            if (id != industry.IndustryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(industry).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IndustryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent(); //204 No Content
+        }
+
+        // POST: api/Industry
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Industry>> PostIndustry(Industry industry)
+        {
+            _context.Industries.Add(industry);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetIndustry", new { id = industry.IndustryId }, industry);
+        }
+
+        // DELETE: api/Industry/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIndustry(int id)
+        {
+            var industry = await _context.Industries.FindAsync(id);
+            if (industry == null)
+            {
+                return NotFound();
+            }
+
+            _context.Industries.Remove(industry);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [NonAction]
+        internal bool IndustryExists(int id)
+        {
+            return _context.Industries.Any(e => e.IndustryId == id);
         }
     }
 }
